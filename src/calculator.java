18e59252d9e8a7e8e548de5f02fe5e890df3424d
Ajay kaarthic J <33996820+ajaykaarthic@users.noreply.github.com>
2025-01-30 22:04:31 -0600

    Create calculator.java

---
+++ src/calculator.java
@@ -0,0 +1,25 @@
+class Calculator {
+    public int divide(int a, int b) {
+        return a / b; // Potential division by zero
+    }
+
+    public int sumArray(int[] numbers) {
+        int sum = 0;
+        for (int i = 0; i <= numbers.length; i++) {
+            sum += numbers[i];
+        }
+        return sum;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        
+        int result = calc.divide(10, 0);
+        System.out.println("Division Result: " + result);
+
+        int[] nums = {1, 2, 3, 4};
+        System.out.println("Sum: " + calc.sumArray(nums));
+    }
+}
